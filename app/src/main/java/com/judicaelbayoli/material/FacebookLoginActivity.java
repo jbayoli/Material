8ae3ce5306d93cbd30af25eb5be16d7a0a1a7e29
Omega240 <omegaosuka@gmail.com>
2018-08-11 20:23:20 +0100

    Omega240

---
+++ app/src/main/java/com/judicaelbayoli/material/FacebookLoginActivity.java
@@ -0,0 +1,13 @@
+package com.judicaelbayoli.material;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FacebookLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_login);
+    }
+}
--- app/src/main/java/com/judicaelbayoli/material/MainActivity.kt
+++ app/src/main/java/com/judicaelbayoli/material/MainActivity.kt
@@ -16,4 +16,9 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, GoogleLoginActivity::class.java)
         startActivity(intent)
     }
+
+    fun facebookLogin(view: View){
+        val intent = Intent(this, FacebookLoginActivity::class.java)
+        startActivity(intent)
+    }
 }
