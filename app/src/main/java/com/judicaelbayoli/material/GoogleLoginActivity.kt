b5abeef43ad8a73c81a55ce044886fda7f04bfbd
jbayoli <31174863+jbayoli@users.noreply.github.com>
2018-08-11 17:46:12 +0100

    Initial commit

---
+++ app/src/main/java/com/judicaelbayoli/material/GoogleLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.judicaelbayoli.material
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GoogleLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_login)
+    }
+}
---
+++ app/src/main/java/com/judicaelbayoli/material/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.judicaelbayoli.material
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun googleLogin(view: View) {
+        val intent = Intent(this, GoogleLoginActivity::class.java)
+        startActivity(intent)
+    }
+}
